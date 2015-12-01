266878491e9ad5c14ff5d3862b73b8d177c4d632
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2015-12-01 05:19:14 +0300

    Disable pre-dexing on the CI

--- ci.sh
+++ ci.sh
@@ -2,4 +2,4 @@
 # Please run it from root project directory
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
-./gradlew clean build findbugs pmd connectedAndroidTest
+./gradlew clean build findbugs pmd connectedAndroidTest -PdisablePreDex
