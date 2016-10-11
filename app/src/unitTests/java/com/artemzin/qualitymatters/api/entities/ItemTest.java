77061d487170cb7c003ec33eabf1f013a196421a
Araz Abishov <araz.abishov.gsoc@gmail.com>
2016-10-11 21:56:34 +0200

    Replaced EqualsVerifier in Item unit tests, which was failing against AutoValue_Item (it was trying to instantiate super type ($AutoValue_Type) with null arguments, as the result .equals() and .hascode() methods were throwing NPE. Suppressing warnings did not help.

--- app/src/unitTests/java/com/artemzin/qualitymatters/api/entities/ItemTest.java
+++ app/src/unitTests/java/com/artemzin/qualitymatters/api/entities/ItemTest.java
@@ -2,18 +2,25 @@ package com.artemzin.qualitymatters.api.entities;
 
 import org.junit.Test;
 
-import nl.jqno.equalsverifier.EqualsVerifier;
-import nl.jqno.equalsverifier.Warning;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class ItemTest {
 
     @Test
-    public void hashCode_equals_shouldWorkCorrectly() {
-        EqualsVerifier
-                .forExamples(
-                        Item.builder().id("id1").imagePreviewUrl("i1").title("Title1").shortDescription("s1").build(),
-                        Item.builder().id("id2").imagePreviewUrl("i2").title("Title2").shortDescription("s2").build())
-                .suppress(Warning.NULL_FIELDS) // AutoValue checks nullability, EqualsVerifier does not expect that by default.
-                .verify();
+    public void equals_shouldWorkCorrectly() {
+        Item itemOne = Item.builder().id("id1").imagePreviewUrl("i1").title("Title1").shortDescription("s1").build();
+        Item itemTwo = Item.builder().id("id2").imagePreviewUrl("i2").title("Title2").shortDescription("s2").build();
+
+        assertThat(itemOne).isNotEqualTo(itemTwo);
+    }
+
+    @Test
+    public void hashcode_shouldWorkCorrectly() {
+        Item itemOne = Item.builder().id("id1").imagePreviewUrl("i1").title("Title1").shortDescription("s1").build();
+        Item itemOneCopy = Item.builder().id("id1").imagePreviewUrl("i1").title("Title1").shortDescription("s1").build();
+        Item itemTwo = Item.builder().id("id2").imagePreviewUrl("i2").title("Title2").shortDescription("s2").build();
+
+        assertThat(itemOne.hashCode()).isEqualTo(itemOneCopy.hashCode());
+        assertThat(itemOne.hashCode()).isNotEqualTo(itemTwo.hashCode());
     }
 }
