0add0d64486fa05d0e155a4f4337560e596cb854
Daniel Gomez Rico <danielgomezrico@gmail.com>
2016-11-25 15:46:14 -0500

    Clean build script (#216)
    Remove unneeded variable

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 set -xe
 
 # You can run it from any directory.
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_DIR=$DIR/
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
 "$PROJECT_DIR"/gradlew --no-daemon --info clean
