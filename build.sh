bfb1fb6cddb498f469502956022e2a48fe9d61a5
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2017-10-21 23:25:14 -0700

    Manually install Android SDK, disable UI tests.

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -xe
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
-"$PROJECT_DIR"/gradlew --no-daemon --info clean
-"$PROJECT_DIR"/gradlew --no-daemon --info build -PdisablePreDex -PwithDexcount -Dscan
-"$PROJECT_DIR"/gradlew --no-daemon --info connectedAndroidTest -PdisablePreDex -PwithDexcount
+"$PROJECT_DIR"/gradlew --no-daemon --info clean build -PdisablePreDex -PwithDexcount -Dscan
+
+# Disable instrumentation tests for now, Travis is too flaky with Android Emulator.
+# See https://github.com/artem-zinnatullin/qualitymatters/issues/220
+# "$PROJECT_DIR"/gradlew --no-daemon --info connectedAndroidTest -PdisablePreDex -PwithDexcount
